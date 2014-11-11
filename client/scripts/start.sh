b5ab53203bc41107164f534c878e7cc8b7adbce7
Gabor Heja <gheja@cubeone>
2014-11-11 22:49:34 +0100

    Client: swapped "ifenslave ..." and "ip link set ... down" as ifenslave forces the link up which is bad

--- client/scripts/start.sh
+++ client/scripts/start.sh
@@ -22,8 +22,8 @@ for number in 0 1 2 3 4 5 6 7 8 9; do
 	TUN_IFACE="tap${number}"
 	
 	openvpn --mktun --dev $TUN_IFACE
-	ip link set $TUN_IFACE down
 	ifenslave bond0 $TUN_IFACE
+	ip link set $TUN_IFACE down
 	
 	sleep 1
 done
